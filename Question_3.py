c95a0ff623d54c2a7690cd5d4fbc789a426dfa43
Shreyas Rathod <sanskrit.112358@gmail.com>
2025-03-09 10:41:57 +0530

    Update Question_3.py

--- Question_3.py
+++ Question_3.py
@@ -27,3 +27,50 @@ def cross_entropy_loss(y_pred, y_true):
     loss_value = -np.sum(y_true * np.log(y_pred_safe)) / num_samples
     return loss_value
 
+
+# Neural Network Class 
+class NeuralNetwork:
+
+    def __init__(self, input_size, hidden_layers, output_size, optimizer, momentum=0.9, rmsprop_beta=0.9, adam_beta1=0.9, adam_beta2=0.999, epsilon=1e-8):
+      
+        self.layer_sizes = [input_size] + hidden_layers + [output_size]
+        self.depth = len(self.layer_sizes)
+        
+        self.optim_type = optimizer.lower()
+        self.m_coef = momentum
+        self.rms_decay = rmsprop_beta
+        self.adam_decay1 = adam_beta1
+        self.adam_decay2 = adam_beta2
+        self.eps = epsilon
+        self.time_step = 0
+        
+        self.initialize_parameters()
+        
+        self.initialize_optimizer_memory()
+
+
+    def initialize_parameters(self):
+        self.weights = []
+        self.biases = []
+        
+        for idx in range(len(self.layer_sizes) - 1):
+            scale = np.sqrt(2.0 / self.layer_sizes[idx])
+            W = np.random.normal(0, scale, (self.layer_sizes[idx], self.layer_sizes[idx+1]))
+            b = np.zeros((1, self.layer_sizes[idx+1]))
+            
+            self.weights.append(W)
+            self.biases.append(b)
+
+
+    def initialize_optimizer_memory(self):
+        self.velocity_w = [np.zeros_like(w) for w in self.weights]
+        self.velocity_b = [np.zeros_like(b) for b in self.biases]
+        
+        self.squared_grad_w = [np.zeros_like(w) for w in self.weights]
+        self.squared_grad_b = [np.zeros_like(b) for b in self.biases]
+        
+        self.moment1_w = [np.zeros_like(w) for w in self.weights]
+        self.moment1_b = [np.zeros_like(b) for b in self.biases]
+        self.moment2_w = [np.zeros_like(w) for w in self.weights]
+        self.moment2_b = [np.zeros_like(b) for b in self.biases]
+
